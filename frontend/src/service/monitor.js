b4b444ae30f038927249bb2e93798d01e8690526
karuapp <104691902+karuapp@users.noreply.github.com>
2025-11-15 10:37:03 -0600

    cd

--- frontend/src/service/monitor.js
+++
@@ -1,8 +0,0 @@
-import request from './request.js'
-
-export function getSystemMetrics() {
-  return request({
-    url: '/metrics',
-    method: 'get'
-  })
-} 
--- frontend/src/service/pm2.js
+++
@@ -1,8 +0,0 @@
-import request from 'src/service/request.js'
-
-export function Restart () {
-  return request({
-    url: `/restart-pm2`,
-    method: 'get'
-  })
-}
